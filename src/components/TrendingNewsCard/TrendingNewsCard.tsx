8b36b28a99a62130cfd05eee6569c390943ff93e
br14nn <vituallabrian727@gmail.com>
2023-05-02 02:39:29 +0800

    Added hover effect in '<a></a>' tag

--- src/components/TrendingNewsCard/TrendingNewsCard.tsx
+++ src/components/TrendingNewsCard/TrendingNewsCard.tsx
@@ -12,7 +12,9 @@ export default function (props: TrendingNewsCardPropos) {
 			<img className="h-auto w-[120px] laptop-base:w-auto" src={props.srcImg} alt={props.imgAlt} />
 			<div className="flex flex-col gap-y-[3.338vw] laptop-base:gap-y-[1.5rem]">
 				<p className="text-[2rem] font-bold leading-none text-grayish-blue laptop-base:text-[4rem]">{props.trendNumber}</p>
-				<p className="text-clamp-custon-3 font-extrabold leading-none laptop-base:text-[1.5rem]">{props.trendTitle}</p>
+				<a href="#" className="text-clamp-custon-3 font-extrabold leading-none text-very-dark-blue hover:text-soft-red laptop-base:text-[1.5rem]">
+					{props.trendTitle}
+				</a>
 				<p className="text-dark-grayish-blue">{props.children}</p>
 			</div>
 		</div>
